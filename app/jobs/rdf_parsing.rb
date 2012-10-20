ebfd787c74358bcacb85b1ab21fe91e3b0f759c4
Danilo Moret <github@moret.pro.br>
2012-10-20 09:59:06 -0300

    refactored rdf parsing job to use 2 spaces identation

--- app/jobs/rdf_parsing.rb
+++ app/jobs/rdf_parsing.rb
@@ -7,13 +7,13 @@ class RDFParsing
   class << self
 
     def perform(name, rdf, segment)
-        rdf_string = `sed -n #{segment[:first] + 1},#{segment[:last] + 1}p #{rdf}`
+      rdf_string = `sed -n #{segment[:first] + 1},#{segment[:last] + 1}p #{rdf}`
 
-        RDF::Reader.for(:ntriples).new(rdf_string) do |reader|
-          reader.each_statement do |statement|
-            parse_line statement.to_triple
-          end
+      RDF::Reader.for(:ntriples).new(rdf_string) do |reader|
+        reader.each_statement do |statement|
+          parse_line statement.to_triple
         end
+      end
     end
 
     def parse_line(s, p, o)

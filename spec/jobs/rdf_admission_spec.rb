8543c9e6a5b4cd139c3816300e9792ea3baa148b
Danilo Moret <github@moret.pro.br>
2012-10-20 09:57:43 -0300

    refactored specs to use 2 spaces identation

--- spec/jobs/rdf_admission_spec.rb
+++ spec/jobs/rdf_admission_spec.rb
@@ -4,13 +4,13 @@ require "./app/jobs/rdf_parsing"
 require "./lib/file_splitter"
 
 describe RDFAdmission do
-    it "should enqueue 3 jobs when splitter splits file in three" do
-        FileSplitter.stub(:split).and_return(Array.new(3) {|i| "fake-file-split-#{i}"})
+  it "should enqueue 3 jobs when splitter splits file in three" do
+    FileSplitter.stub(:split).and_return(Array.new(3) {|i| "fake-file-split-#{i}"})
 
-        Resque.should_receive(:enqueue).with(RDFParsing, "test_paper", "paper.nt", 'fake-file-split-0').and_return(true)
-        Resque.should_receive(:enqueue).with(RDFParsing, "test_paper", "paper.nt", 'fake-file-split-1').and_return(true)
-        Resque.should_receive(:enqueue).with(RDFParsing, "test_paper", "paper.nt", 'fake-file-split-2').and_return(true)
+    Resque.should_receive(:enqueue).with(RDFParsing, "test_paper", "paper.nt", 'fake-file-split-0').and_return(true)
+    Resque.should_receive(:enqueue).with(RDFParsing, "test_paper", "paper.nt", 'fake-file-split-1').and_return(true)
+    Resque.should_receive(:enqueue).with(RDFParsing, "test_paper", "paper.nt", 'fake-file-split-2').and_return(true)
 
-        described_class.perform("test_paper", "paper.nt")
-    end
+    described_class.perform("test_paper", "paper.nt")
+  end
 end
--- spec/jobs/rdf_parsing_spec.rb
+++ spec/jobs/rdf_parsing_spec.rb
@@ -1,12 +1,12 @@
 require "./app/jobs/rdf_parsing"
 
 describe RDFParsing, "parser" do
-    paper_file = "spec/assets/paper.nt"
+  paper_file = "spec/assets/paper.nt"
 
-    it "should be thoroughly tested"
+  it "should be thoroughly tested"
 
-    it "should parse each line of of the file" do
-        RDFParsing.should_receive(:parse_line).exactly(12)
-        described_class.perform("paper", paper_file, {first: 0, last: 11})
-    end
+  it "should parse each line of of the file" do
+    RDFParsing.should_receive(:parse_line).exactly(12)
+    described_class.perform("paper", paper_file, {first: 0, last: 11})
+  end
 end

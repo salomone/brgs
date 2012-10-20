a51d6b89b5067672a7af30e26a4166e031a1693d
Danilo Moret <github@moret.pro.br>
2012-10-20 11:53:00 -0300

    redefined redis connection to be used as include

--- spec/lib/redis_connection_spec.rb
+++ spec/lib/redis_connection_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 
 describe RedisConnection do
   class DummyUsingRedisConnection
-    extend RedisConnection
+    include RedisConnection
   end
 
   it "returns a namespaced connection based on rack environment" do
     old_rack_env = ENV['RACK_ENV']
     ENV['RACK_ENV'] = 'bogus_rack_env_test'
 
-    DummyUsingRedisConnection.redis.namespace.should eq "bogus_rack_env_test:bgrs"
+    DummyUsingRedisConnection.new.redis.namespace.should eq "bogus_rack_env_test:bgrs"
 
     ENV['RACK_ENV'] = old_rack_env
   end

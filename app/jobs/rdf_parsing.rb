29760f99edf6336c9489b301f4712fe9a7b17939
Danilo Moret <github@moret.pro.br>
2012-12-10 18:10:48 -0200

    skipped use of RDF::Reader for performance

--- app/jobs/rdf_parsing.rb
+++ app/jobs/rdf_parsing.rb
@@ -16,14 +16,19 @@ class RDFParsing
 
   def perform
     rdf_string = redis.hget 'segments', @rdf_segment
-    RDF::Reader.for(:ntriples).new(rdf_string) do |reader|
-      reader.each_statement do |statement|
-        parse_line(*statement.to_triple)
+    rdf_string.each_line do |line|
+      matches = line.match(/<(.*)> <(.*)> ([<"])(.*)[>"] \./)
+      if matches
+        s = matches[1]
+        p = matches[2]
+        o = matches[4]
+        o_literal = matches[3] == '"'
+        parse_line(s, p, o, o_literal)
       end
     end
   end
 
-  def parse_line(s, p, o)
+  def parse_line(s, p, o, o_literal)
     si, sc = index 'node', s
     pi = index('edge', p)[0]
     oi, oc = index 'node', o
@@ -33,7 +38,7 @@ class RDFParsing
     redis.sadd 'sources', si if sc
     redis.srem 'sources', oi
 
-    redis.sadd 'sinks', oi if oc
+    redis.sadd 'sinks', oi if oc && o_literal
     redis.srem 'sinks', si
   end
 

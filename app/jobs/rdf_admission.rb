ba4dc25e96311cee50c34052225b201980e0cad0
Danilo Moret <github@moret.pro.br>
2013-03-16 12:30:47 -0300

    created temporary file for rdf admission

--- app/jobs/rdf_admission.rb
+++ app/jobs/rdf_admission.rb
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
+require 'tempfile'
+
 class RDFAdmission
   extend RedisConnection
 
   @queue = :rdf_admission
 
-  def self.perform(rdf)
+  def self.perform rdf_content
     BRGS::Parser.destroy_indexes
+
+    rdf_file = Tempfile.new name
+    rdf_file.write rdf_content
+    rdf = rdf_file.path
+    rdf_file.close
 
     FileSplitter.segment(rdf).each do |segment|
       piece = FileSplitter.piece rdf, segment
